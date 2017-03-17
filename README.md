7ae512703032206efc09a6f3c37d212718bc919b
Will Hall <will@designspike.com>
2017-03-17 11:11:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Search Helper Extension for Bolt CMS
+====================================
+
+Normally, repeater fields aren't searchable with Bolt's built in search function. This extension works around that by dumping all your repeater fields' text values into a field of your choice whenever you save.
+
+**It requires configuration though** - modify the example config file that gets created at extensions/search-helper.designspike.yml for a quick start. You only need to pick your contenttypes, and the destination field for each one.
---
+++ src/SearchHelperExtension.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Bolt\Extension\DesignSpike\SearchHelper;
+
+use Bolt\Events\StorageEvent;
+use Bolt\Events\StorageEvents;
+use Bolt\Extension\SimpleExtension;
+use Bolt\Storage\Mapping\ContentType;
+use Bolt\Storage\Entity\Content;
+
+class SearchHelperExtension extends SimpleExtension
+{
+    public static function getSubscribedEvents()
+    {
+        $localEvents = [
+            StorageEvents::PRE_SAVE => [
+                ['onPreSave', 0]
+            ]
+        ];
+        return  $localEvents + parent::getSubscribedEvents();
+    }
+
+    /**
+     * Save the string values of a repeater field to a specified keywords field.
+     */
+    public function onPreSave(StorageEvent $event)
+    {
+        $config = $this->getConfig();
+
+        $globArr = []; // we wil temporarily store the globbed together keywords here
+
+        /** @var Content $subject */
+        $subject = $event->getSubject();
+
+        if (!$subject instanceof Content) {
+            return;
+        }
+
+        /** @var ContentType $contentType */
+        $contentType = $subject->getContenttype();
+
+        $fields = $contentType->getFields();
+
+        // do we even have any contenttypes specified in this extension's config?
+        $contentTypeSlug = $contentType->offsetGet('slug');
+        if (!isset($config['contenttypes']) or !is_array($config['contenttypes'])) {
+            return;
+        }
+
+        // is this one of the contenttypes that is specified in the extension's config?
+        if (!array_key_exists($contentTypeSlug, $config['contenttypes'])) {
+            return;
+        }
+
+        // does the config specify a field to store the keywordfield?
+        if (!array_key_exists('keywordfield', $config['contenttypes'][$contentTypeSlug])) {
+            return;
+        }
+
+        // ensure that the field where the keywords will be stored actually exists
+        $keywordField = $config['contenttypes'][$contentTypeSlug]['keywordfield'];
+        if (!array_key_exists($keywordField, $fields)) {
+            return;
+        }
+
+        // ensure the field is a string type
+        if (!in_array($fields[$keywordField]['type'], ['textarea', 'text', 'html'])) {
+            return;
+        }
+
+        foreach ($fields as $fieldName => $field) {
+            // we only care about repeaters
+            if ($field['type'] !== 'repeater') {
+                continue;
+            }
+            // loop through each index of the repeater
+            foreach ($subject->get($fieldName) as $item) {
+                foreach($item as $value) {
+                    if (is_string($value)) {
+                        // ignore json
+                        json_decode($value);
+                        if (JSON_ERROR_NONE===json_last_error()) {
+                            continue;
+                        }
+                        $globArr[] = strip_tags($value);
+                    }
+                }
+            }
+        }
+
+        // glob the strings together, then clean up and save
+        $globStr = join(" ", $globArr);
+        $globStr = trim(preg_replace("@\s+@", " ", $globStr));
+
+        $subject->set($keywordField, $globStr);
+    }
+
+    /**
+     * Set up the default configuration.
+     *
+     * @see https://docs.bolt.cm/extensions/basics/configuration#providing-defaults
+     *
+     * @return array
+     */
+    protected function getDefaultConfig()
+    {
+        return [
+            'contenttypes' => [
+                'mycontenttypeslug' => [
+                    'keywordfield' => 'myfieldname',
+                ],
+                'anothercontenttypeslug' => [
+                    'keywordfield' => 'anotherfieldname'
+                ]
+            ]
+        ];
+    }
+
+}
